42f767e60085914a5961c9359ae6c920956cef5b
pimka <zelenova.yulia@mail.ru>
2019-09-08 10:45:53 +0300

    changed hosts

--- rsoi1/settings.py
+++ rsoi1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u_i4605#p*n4$5xorf)57it(ipr$aa
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['glacial-wave-39358.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
